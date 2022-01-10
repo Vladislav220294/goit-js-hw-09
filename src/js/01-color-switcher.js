dcc493d64321a3a6545e772078e281bb69a12d45
Vladislav220294 <vladislav22021994@gmail.com>
2022-01-10 17:59:22 +0300

    исправляет первую задачу

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -6,18 +6,16 @@ const startBtn = document.querySelector('button[data-start]');
 const stopBtn = document.querySelector('button[data-stop]');
 const bodyEl = document.querySelector('body');
 let colorId = null;
-let isActive = false;
+
 startBtn.addEventListener('click', onStartBtn);
 stopBtn.addEventListener('click', onStopBtn);
 
 function onStartBtn() {
-  if (isActive) {
-    return;
-  }
-  isActive = true;
+  startBtn.setAttribute('disabled', '');
   colorId = setInterval(() => (bodyEl.style.backgroundColor = getRandomHexColor()), 1000);
 }
 function onStopBtn() {
   clearInterval(colorId);
-  isActive = false;
+
+  startBtn.removeAttribute('disabled');
 }
